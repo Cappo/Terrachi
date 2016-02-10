a2246dc11d6b3fe76d088cba79ba6b4be13c3877
RyanCross <studiopod.RyanCross@outlook.com>
2016-02-10 17:39:24 -0600

    set-up working for raycasting

---
+++ Terrachi/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    //skin width inset for raycasting
+    const float skinWidth = .015f; 
+
+    //define how many rays are being fired horizontally and vertically:
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    //define the spacing between each horizontal/vertical ray, depending on how many we've chosen to fire + size of the bounds
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+
+    BoxCollider2D collider; 
+    RaycastOrigins raycastOrigins;
+
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateRaycastOrigins();
+        CalculateRaySpacing();
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+        }
+	}
+
+    void UpdateRaycastOrigins() {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2); //shrinks bounds on all sides by skinWidth
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+
+    }
+
+    void CalculateRaySpacing() {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2); //shrinks bounds on all sides by skinWidth
+
+        //these lines ensure that atleast 2 rays are being fired in the horizontal & vertical directions
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        //calculate spacing between each ray
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+
+    }
+
+    //struct to store the four corners of our box collider (at all times).
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    } 
+    
+}
---
+++ Terrachi/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))] //automically adds Controller2D upon attaching script to gameObject
+public class Player : MonoBehaviour {
+
+
+    Controller2D controller; //reference to the controller
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<Controller2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
