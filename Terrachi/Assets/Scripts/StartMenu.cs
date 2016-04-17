f716be39608a76979816ea97c109232034a089a6
RyanCross <studiopod.RyanCross@outlook.com>
2016-04-17 16:27:10 -0500

    added start menu and start menu script. Functions NewGame(), LoadGame(), QuitGame() tied to their respective buttons

---
+++ Terrachi/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+    public string startLevel;
+
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(startLevel);
+    }
+
+    public void LoadGame()
+    {
+        //TO DO
+
+
+    }
+
+    public void QuitGame()
+    {
+
+
+        Application.Quit();
+    }
+
+}
